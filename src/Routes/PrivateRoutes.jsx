e77b1e0cafb287f1e8fe8de605db5f99b0746db9
Solaimanislam <islamsi2001@gmail.com>
2024-05-15 15:04:05 +0600

    fixed provider error

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
-// import { useContext } from 'react';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import { AuthContext } from '../pages/Providers/AuthProvider';
+
 
 const PrivateRoutes = ({children}) => {
 
